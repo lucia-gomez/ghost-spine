ed096fe9cb44cdc5c7324ca95adf2c3e5e843fbf
lucia <51058748+lucia-gomez@users.noreply.github.com>
2023-10-13 12:25:54 -0400

    add eval node in TD

--- stretch-test/stretch-test.ino
+++ stretch-test/stretch-test.ino
@@ -4,10 +4,15 @@ void setup() {
   pinMode(A0, INPUT);
 }
 
+// float mapFloat(int value, int start, int end, int mapStart, int mapEnd) {
+//   return (float)(value - start) * (mapEnd - mapStart) / (float)(end - start) + mapStart;
+// }
+
 void loop() {
   // put your main code here, to run repeatedly:
   int sensorValue = analogRead(A0);
   // Serial.println(sensorValue);
-  int speed = map(sensorValue, 400, 140, 1, 9);
+  int speed = map(sensorValue, 280, 170, 1, 9);
   Serial.println(speed);
+  delay(50);
 }
